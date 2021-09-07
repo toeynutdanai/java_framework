7ee958baf4e15c62af4386d8938da66a23f48808
KBANKNET\natdanai.k <natdanai.k@kbtg.tech>
2021-09-07 13:57:23 +0700

    test with mock service failure

--- demoapp/src/test/java/com/example/demoapp/employees/EmployeeControllerServiceTests.java
+++ demoapp/src/test/java/com/example/demoapp/employees/EmployeeControllerServiceTests.java
@@ -13,18 +13,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
 import com.example.demoapp.employees.EmployeeResponse;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class EmployeeControllerServiceTests {
 
-	@Autowired 
+	@Autowired
 	private TestRestTemplate restTemplate;
-	
+
 	@MockBean
 	private EmployeeRepository employeeRepository;
-	
+
 	@Test
 	@DisplayName("Success case")
 	public void case01() {
@@ -35,12 +36,26 @@ class EmployeeControllerServiceTests {
 		employee.setName("natdanai");
 		when(employeeRepository.findById(1)).thenReturn(Optional.of(employee));
 		// Action
-		EmployeeResponse result 
-			= restTemplate.getForObject("/employees/" + id, EmployeeResponse.class);
-	
-		//Assert
+		EmployeeResponse result = restTemplate.getForObject("/employees/" + id, EmployeeResponse.class);
+
+		// Assert
 		assertEquals(id, result.getId());
 		assertEquals("natdanai", result.getName());
+	}
+
+	@Test
+	@DisplayName("Failure case :: Employee not found = 100")
+	public void case02() {
+		// Arrange
+		int id = 100;
+		when(employeeRepository.findById(100)).thenReturn(Optional.empty());
+		// Action
+		ResponseEntity<ErrorResponse> result = restTemplate.getForEntity("/employees/" + id, ErrorResponse.class);
+		
+		// Assert
+		assertEquals(404, result.getStatusCodeValue());
+		assertEquals(404, result.getBody().getCode());
+		assertEquals("Employee not found id=100", result.getBody().getDetail());
 	}
 
 }
