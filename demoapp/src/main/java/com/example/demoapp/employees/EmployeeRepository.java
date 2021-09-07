de20b7ea6da2ac5fba55ab08897af985bdd1bdba
KBANKNET\natdanai.k <natdanai.k@kbtg.tech>
2021-09-07 09:35:05 +0700

    create repository

---
+++ demoapp/src/main/java/com/example/demoapp/employees/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.example.demoapp.employees;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+
+}
